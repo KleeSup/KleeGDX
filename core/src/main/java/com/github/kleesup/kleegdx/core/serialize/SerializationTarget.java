a8db23b48198d78d571c6f3a51a55f2e513850ea
KleeSup <bernd_brot.1@web.de>
2024-10-19 13:37:57 +0200

    Added JavaDoc to SerializationTarget

--- core/src/main/java/com/github/kleesup/kleegdx/core/serialize/SerializationTarget.java
+++ core/src/main/java/com/github/kleesup/kleegdx/core/serialize/SerializationTarget.java
@@ -1,13 +1,21 @@
 package com.github.kleesup.kleegdx.core.serialize;
 
+/**
+ * Denotes what a class (or method) in a serialization context should be used for. This could be either:
+ * <l>
+ *     <li>NET: Should only be used for packet serialization, often version dependent</li>
+ *     <li>STORING: Should only be used for storing data to drives or databases, usually version independent</li>
+ *     <li>BOTH: Most optimal, freely to use for both storing and net packets, should be version independent</li>
+ * </l>
+ */
 public @interface SerializationTarget {
 
-    Type target() default Type.NET;
+    Type target();
 
     enum Type{
         NET,
         STORING,
-        BOTH;
+        BOTH
     }
 
 }
